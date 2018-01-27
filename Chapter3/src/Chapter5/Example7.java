e85a31cac7dcf79a33ff88f934c99fc9b4a000ef
shengwenqi <1584674061@qq.com>
2018-01-27 22:23:51 +0800

    t

---
+++ Chapter3/src/Chapter5/Example7.java
@@ -0,0 +1,9 @@
+package Chapter5;
+import java.io.*;
+
+public class Example7 {
+    public static void main(String[] args) throws Exception
+    {
+        BufferedInputStream bufferInput = new BufferedInputStream(new FileInputStream("./src/woman.jpg"));
+    }
+}
